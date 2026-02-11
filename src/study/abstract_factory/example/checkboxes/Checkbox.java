771fec33e5fc610eba6d1253829f3016e28781bd
Jusley Souza <jusleysouza31@gmail.com>
2026-02-11 08:46:41 -0300

    Adding a common interface for the second product family.

---
+++ src/study/abstract_factory/example/checkboxes/Checkbox.java
@@ -0,0 +1,6 @@
+package study.abstract_factory.example.checkboxes;
+
+//As caixas de selecao sao a segunda familia de produtos. Elas possuem as mesmas variantes que os botoes.
+public interface Checkbox {
+	void paint();
+}
