058b1d3a501125dde05f591c93dda83b6bb8e9db
Jusley Souza <jusleysouza31@gmail.com>
2026-02-18 15:15:31 -0300

    Adding the class responsible for building a user manual for a car.

---
+++ src/study/builder/example/builders/CarManualBuilder.java
@@ -0,0 +1,52 @@
+package study.builder.example.builders;
+
+import study.builder.example.cars.CarType;
+import study.builder.example.cars.Manual;
+import study.builder.example.components.Engine;
+import study.builder.example.components.GPSNavigator;
+import study.builder.example.components.Transmission;
+import study.builder.example.components.TripComputer;
+
+//Classe responsavel por construir um manual do usuario para um carro.
+public class CarManualBuilder implements Builder{
+    private CarType type;
+    private int seats;
+    private Engine engine;
+    private Transmission transmission;
+    private TripComputer tripComputer;
+    private GPSNavigator gpsNavigator;
+
+    @Override
+    public void setCarType(CarType type) {
+        this.type = type;
+    }
+
+    @Override
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    @Override
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public void setTransmission(Transmission transmission) {
+        this.transmission = transmission;
+    }
+
+    @Override
+    public void setTripComputer(TripComputer tripComputer) {
+        this.tripComputer = tripComputer;
+    }
+
+    @Override
+    public void setGPSNavigator(GPSNavigator gpsNavigator) {
+        this.gpsNavigator = gpsNavigator;
+    }
+
+    public Manual getResult() {
+        return new Manual(type, seats, engine, transmission, tripComputer, gpsNavigator);
+    }
+}
